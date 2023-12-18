862b8d8b01fb5d3adde926b202990386ff4a7e78
Umair-Zubair <umair123zubair@gmail.com>
2023-12-19 02:30:52 +0500

    update

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include "gameState.hpp"
 #include "showHealth.hpp"
 #include "music.hpp"
+#include "frame.hpp"
 
 using namespace std;
 // g++ *.cpp -IC:\mingw_dev_lib\include\SDL2 -LC:\mingw_dev_lib\lib -w -lmingw32 -lSDL2main -lSDL2 -lSDL2_image -o main
